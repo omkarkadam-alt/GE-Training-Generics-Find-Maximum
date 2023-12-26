75f8e17c55713c9644299a33905bf8a884507123
Omkar Kadam <kadam.omkar@outlook.com>
2023-12-27 03:42:55 +0530

    Implement for String Class

--- src/main/java/generics_find_maximum/GetMaximum.java
+++ src/main/java/generics_find_maximum/GetMaximum.java
@@ -12,4 +12,9 @@ public class GetMaximum {
         return (float1.compareTo(float2) > 0 ? (float1.compareTo(float3) > 0 ? float1: float3): (float2.compareTo(float3) > 0 ? float2: float3));
     }
 
+    static String getMaximum(String string1, String string2, String string3){
+
+        return (string1.compareTo(string2) > 0 ? (string1.compareTo(string3) > 0 ? string1: string3): (string2.compareTo(string3) > 0 ? string2: string3));
+    }
+
 }
--- src/main/java/generics_find_maximum/Main.java
+++ src/main/java/generics_find_maximum/Main.java
@@ -4,21 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         // Test Case - 1
-        Float float1 = (float) 20.82;
-        Float float2 = (float) 1.24;
-        Float float3 = (float) 10.65;
-        System.out.println("Maximum of (" + Float.valueOf(float1) + " " + Float.valueOf(float2) + " " + Float.valueOf(float3) + ") is " + GetMaximum.getMaximum(float1, float2, float3));
+        String string1 = "Orange";
+        String string2 = "Apple";
+        String string3 = "Banana";
+        System.out.println("Maximum of (" + String.valueOf(string1) + " " + String.valueOf(string2) + " " + String.valueOf(string3) + ") is " + GetMaximum.getMaximum(string1, string2, string3));
 
         // Test Case - 2
-        Float float4 = (float) 20.82;
-        Float float5 = (float) 100.24;
-        Float float6 = (float) 10.65;
-        System.out.println("Maximum of (" + Float.valueOf(float4) + " " + Float.valueOf(float5) + " " + Float.valueOf(float6) + ") is " + GetMaximum.getMaximum(float4, float5, float6));
+        String string4 = "Apple";
+        String string5 = "Cherry";
+        String string6 = "Banana";
+        System.out.println("Maximum of (" + String.valueOf(string4) + " " + String.valueOf(string5) + " " + String.valueOf(string6) + ") is " + GetMaximum.getMaximum(string4, string5, string6));
 
         // Test Case - 3
-        Float float7 = (float) 2.82;
-        Float float8 = (float) 1.24;
-        Float float9 = (float) 10.65;
-        System.out.println("Maximum of (" + Float.valueOf(float7) + " " + Float.valueOf(float8) + " " + Float.valueOf(float9) + ") is " + GetMaximum.getMaximum(float7, float8, float9));
+        String string7 = "Apple";
+        String string8 = "Banana";
+        String string9 = "Peach";
+        System.out.println("Maximum of (" + String.valueOf(string7) + " " + String.valueOf(string8) + " " + String.valueOf(string9) + ") is " + GetMaximum.getMaximum(string7, string8, string9));
     }
 }
