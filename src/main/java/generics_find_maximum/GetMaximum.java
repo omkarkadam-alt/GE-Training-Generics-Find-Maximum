ec62a12118c9d393121c28adc3a465d15e447455
Omkar Kadam <kadam.omkar@outlook.com>
2023-12-27 03:34:26 +0530

    Implement for Float Class

--- src/main/java/generics_find_maximum/GetMaximum.java
+++ src/main/java/generics_find_maximum/GetMaximum.java
@@ -7,4 +7,9 @@ public class GetMaximum {
         return (num1.compareTo(num2) > 0 ? (num1.compareTo(num3) > 0 ? num1: num3): (num2.compareTo(num3) > 0 ? num2: num3));
     }
 
+    static Float getMaximum(Float float1, Float float2, Float float3){
+       
+        return (float1.compareTo(float2) > 0 ? (float1.compareTo(float3) > 0 ? float1: float3): (float2.compareTo(float3) > 0 ? float2: float3));
+    }
+
 }
--- src/main/java/generics_find_maximum/Main.java
+++ src/main/java/generics_find_maximum/Main.java
@@ -4,21 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
         // Test Case - 1
-        Integer int1 = 20;
-        Integer int2 = 1;
-        Integer int3 = 10;
-        System.out.println("Maximum of (" + Integer.valueOf(int1) + " " + Integer.valueOf(int2) + " " + Integer.valueOf(int3) + ") is " + GetMaximum.getMaximum(int1, int2, int3));
+        Float float1 = (float) 20.82;
+        Float float2 = (float) 1.24;
+        Float float3 = (float) 10.65;
+        System.out.println("Maximum of (" + Float.valueOf(float1) + " " + Float.valueOf(float2) + " " + Float.valueOf(float3) + ") is " + GetMaximum.getMaximum(float1, float2, float3));
 
         // Test Case - 2
-        Integer int4 = 20;
-        Integer int5 = 100;
-        Integer int6 = 10;
-        System.out.println("Maximum of (" + Integer.valueOf(int4) + " " + Integer.valueOf(int5) + " " + Integer.valueOf(int6) + ") is " + GetMaximum.getMaximum(int4, int5, int6));
+        Float float4 = (float) 20.82;
+        Float float5 = (float) 100.24;
+        Float float6 = (float) 10.65;
+        System.out.println("Maximum of (" + Float.valueOf(float4) + " " + Float.valueOf(float5) + " " + Float.valueOf(float6) + ") is " + GetMaximum.getMaximum(float4, float5, float6));
 
         // Test Case - 3
-        Integer int7 = 2;
-        Integer int8 = 1;
-        Integer int9 = 10;
-        System.out.println("Maximum of (" + Integer.valueOf(int7) + " " + Integer.valueOf(int8) + " " + Integer.valueOf(int9) + ") is " + GetMaximum.getMaximum(int7, int8, int9));
+        Float float7 = (float) 2.82;
+        Float float8 = (float) 1.24;
+        Float float9 = (float) 10.65;
+        System.out.println("Maximum of (" + Float.valueOf(float7) + " " + Float.valueOf(float8) + " " + Float.valueOf(float9) + ") is " + GetMaximum.getMaximum(float7, float8, float9));
     }
 }
